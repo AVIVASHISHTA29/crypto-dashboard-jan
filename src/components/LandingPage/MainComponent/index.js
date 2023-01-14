2d308b7f8c0ff47b4da76461fa3fd49ee303b95e
avivashishta29 <avivashishta@gmail.com>
2023-01-14 20:54:37 +0530

    framer motion

--- src/components/LandingPage/MainComponent/index.js
+++ src/components/LandingPage/MainComponent/index.js
@@ -3,25 +3,61 @@ import Button from "../../Common/Button";
 import "./styles.css";
 import gradient from "../../../assets/gradient.png";
 import iphone from "../../../assets/iphone.png";
+import { motion } from "framer-motion";
 
 function MainComponent() {
   return (
     <div className="main-flex">
       <div className="info-landing">
-        <h1 className="heading1">Track Crypto</h1>
-        <h1 className="heading2">Real Time.</h1>
-        <p className="info-text">
+        <motion.h1
+          className="heading1"
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ delay: 0.5, duration: 1 }}
+        >
+          Track Crypto
+        </motion.h1>
+        <motion.h1
+          className="heading2"
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ delay: 0.75, duration: 1 }}
+        >
+          Real Time.
+        </motion.h1>
+        <motion.p
+          className="info-text"
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ delay: 1, duration: 1 }}
+        >
           Track crypto through a public api in real time. Visit the dashboard to
           do so!{" "}
-        </p>
-        <div className="btn-flex">
+        </motion.p>
+        <motion.div
+          className="btn-flex"
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0, duration: 1 }}
+        >
           <Button text={"Dashboard"} />
           <Button text={"Share App"} outlined={true} />
-        </div>
+        </motion.div>
       </div>
       <div className="gradient-div">
         <img src={gradient} className="gradient" />
-        <img src={iphone} className="iphone" />
+        <motion.img
+          src={iphone}
+          className="iphone"
+          initial={{ y: -10 }}
+          animate={{ y: 10 }}
+          transition={{
+            type: "smooth",
+            repeatType: "mirror",
+            duration: 2,
+            repeat: Infinity,
+          }}
+        />
       </div>
     </div>
   );
